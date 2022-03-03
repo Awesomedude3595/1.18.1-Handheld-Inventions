4e93072df704287c39e15b87735dc80dbc9f6cb2
Awesomedude3595 <100804189+Awesomedude3595@users.noreply.github.com>
2022-03-03 16:03:51 +0000

    A

--- src/main/java/net/awesomedude3595/handheld_inventions/HandheldInventions.java
+++ src/main/java/net/awesomedude3595/handheld_inventions/HandheldInventions.java
@@ -1,13 +1,7 @@
 package net.awesomedude3595.handheld_inventions;
 
-import net.awesomedude3595.handheld_inventions.client.screen.EssenceConverterScreen;
-import net.awesomedude3595.handheld_inventions.core.event.ClientSetup;
 import net.awesomedude3595.handheld_inventions.core.init.*;
-import net.minecraft.client.gui.screens.MenuScreens;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod("handheld_inventions")
@@ -23,7 +17,5 @@ public class HandheldInventions {
         BlockEntityTypes.BLOCK_ENTITIES.register(bus);
         ContainerInit.CONTAINERS.register(bus);
         MenuTypes.MENU_TYPES.register(bus);
-
-        MinecraftForge.EVENT_BUS.register(ClientSetup.class);
     }
 }
